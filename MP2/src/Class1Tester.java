35c192699e3fa854571a45007020fae9005129f8
s1210287 <s1210287@HH306-09WL55882.monmouth.edu>
2020-02-05 19:48:29 -0500

    1

---
+++ MP2/src/Class1Tester.java
@@ -0,0 +1,9 @@
+
+public class Class1Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MP2/src/Class2Tester.java
@@ -0,0 +1,9 @@
+
+public class Class2Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
